e151b091a4971e555f638efdb3ddac4bf7afb2ac
John Doe <johndoe@example.com>
2021-09-26 14:29:44 +0800

    第四次提交，修改run_suite.py 2021/9/26 14:29

--- run/run_suite.py
+++ run/run_suite.py
@@ -1,5 +1,7 @@
+import sys
+sys.path.append(r"C:/Users/Administrator/PycharmProjects/api_automatic")
+
 import os
-import time
 import unittest
 import HTMLTestRunner
 from script.login import LoginApi
@@ -7,7 +9,8 @@ from script.login import LoginApi
 suite = unittest.TestSuite()
 suite.addTest(unittest.makeSuite(LoginApi))
 report_path = os.path.abspath("..")+"/report/"
-report_file = open(report_path+"hy_api_report_"+time.strftime("%Y-%m-%d", time.localtime())+".html", "wb")
+# report_file = open(report_path+"hy_api_report_"+time.strftime("%Y-%m-%d", time.localtime())+".html", "wb")
+report_file = open(report_path+"hy_api_report"+".html", "wb")
 runner = HTMLTestRunner.HTMLTestRunner(stream=report_file, title="华翼接口测试", description="华翼登录接口测试", verbosity=3)
 
 runner.run(suite)
